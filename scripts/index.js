9eefa673f7ed59e2689ec315e4c6513af089581f
fronti <lu4sche@list.ru>
2024-05-09 17:20:39 +0300

    ПР5. 1-я попытка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,63 @@
-// @todo: Темплейт карточки
+// TODO: Темплейт карточки
+// получаю содержимое template
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
+// TODO: DOM узлы
+// получаю объект, принадлежащий <main>
+const mainContent = document.querySelector('.content');
+// получаю объект-место, куда буду вставлять карточки
+const cardsContainer = mainContent.querySelector('.places__list');
 
-// @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+// TODO: Функция создания ОДНОЙ карточки
+const createCard = (initialCards, deleteCard) => {
+  
+  // получаю объект, включающий фото, кнопку-корзину, заголовок, кнопку-лайк
+  // клонирую со всеми потомками методом (cloneNode(true))
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  // к найденным объектам добавляю информацию из массива карточек (файл cards.js)
+  cardElement.querySelector('.card__title').textContent = initialCards.name;
+  cardElement.querySelector('.card__image').src = initialCards.link;
+  cardElement.querySelector('.card__image').alt = initialCards.name;
+  
+  // получаю кнопку-корзину, которая удаляет карточку
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+
+  // вешаю слушатель на кнопку-корзину
+  // 2-й параметр - функция-колбэк, которая сработает при клике и удалит карточку
+  cardDeleteButton.addEventListener('click', deleteCard);
+
+  // результат работы функции - подготовленная к выводу карточка. Карточка пока не выведена.
+  return cardElement;
+}
+
+
+// TODO: Функция удаления ОДНОЙ карточки
+function deleteCard() {
+
+  // нахожу бокс карточки (картинка, заголовок, кнопка-корзина, кнопка-лайк) по второму значению в классе
+  const card = mainContent.querySelector('.card');
+  card.remove(); //убираю карточку
+}
+
+
+// TODO: Вывести карточки на страницу
+function showCards(initialCards) {
+
+  // перебираю массив с карточками (initialCards)
+  // при forEach функция будет вызвана на каждом элементе массива поочерёдно
+  initialCards.forEach(function (item) {
+
+  // 1-й параметр - данные карточек из initialCards,
+  // 2-ой параметр - функция удаления карточки
+    const cardItem = createCard(item, deleteCard);
+
+    // добавляю карточку/и в конец cardsContainer
+    cardsContainer.append(cardItem);
+  })
+}
+
+// вывожу массив карточек на страницу, вызывая функцию
+showCards(initialCards);
